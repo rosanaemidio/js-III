9151b5981da3b13405dec9c0c97a71aa06382a0e
rosanaemidio <rosanaemidio42@gmail.com>
2019-10-30 17:15:57 -0300

    minha resolução do exercicio

--- 01b_ajax_json/index.js
+++ 01b_ajax_json/index.js
@@ -8,3 +8,91 @@
 // [] iterar sobre o array e mostrar as três imagens usando for()
 // [] iterar sobre o array e mostrar as três imagens usando for...of
 // [] iterar sobre o array e mostrar as três imagens usando forEach
+
+// === Como eu resolvi a primeira parte
+
+// const request= new XMLHttpRequest ()
+// const url = './data/dados.json'
+
+// request.onreadystatechange = mostraImg; //
+// request.open('GET', url); //manda a requisição 
+// request.send(); // recebe a resposta
+
+// function mostraImg(){
+//     if(request.status === 200 && request.readyState === 4){
+//         const response = request.response;
+//         const json = JSON.parse(response);
+//         const data = json.data;
+//         console.log(data[0].imagem);
+
+//         const div = document.querySelector('#root');
+//         const body = document.body
+//         body.innerHTML += `
+//         <img src=${json.data[0].imagem}>
+//         <img src=${json.data[1].imagem}>
+//         <img src=${json.data[2].imagem}>
+//         `
+// } else {
+
+// }
+//     }
+
+
+    //Resposta do exercício For e ForEach
+    const div = document.querySelector('#root');
+const url = './data/dados.json';
+
+const req = new XMLHttpRequest();
+req.open('GET', url);
+req.send();
+
+req.onreadystatechange = pegarDados;
+
+function pegarDados() {
+  if (req.readyState === 4) {
+    if (req.status === 200) {
+      const response = req.response;
+      const json = JSON.parse(response);
+      const data = json.data;
+      // criarImagens(data);
+      // criarImagensFor(data);
+      // criarImagensForEach(data);
+      criarImagensForOf(data);
+    }
+  }
+}
+
+
+function criarImagens(array) {
+  div.innerHTML += `
+  <img src=${array[0].imagem} >
+  <img src=${array[1].imagem} >
+  <img src=${array[2].imagem} >
+  `
+}
+
+function criarImagensFor(array) {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    const img = `<img src=${element.imagem}>`;
+    div.innerHTML += img
+  }
+}
+
+function criarImagensForEach(array) {
+  array.forEach(element => {
+    console.log(element);
+    const img = `<img src=${element.imagem}>`;
+    div.innerHTML += img;
+  });
+}
+
+function criarImagensForOf(array) {
+  for (const posicao of array) {
+    // const img = `<img src=${posicao.imagem}>`;
+    // div.innerHTML = img;
+    const img = document.createElement('img');
+    img.src = posicao.imagem;
+    div.appendChild(img);
+  }
+}
---
+++ 01f_ajax_giphy_search_input/index.js
@@ -0,0 +1,10 @@
+// XMLHttpRequest na rota search capturando informação do input
+// trazer 10 imagens
+// apresentar as imagens na div #root
+
+//Pegar o value do input
+
+
+const request = new XMLHttpRequest()
+const input = document.getElementById('gifInput')
+console.log('input')
