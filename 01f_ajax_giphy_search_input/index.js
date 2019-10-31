9facd5eafd365eddbb9a72f1dc257c3a950862e3
rosanaemidio <rosanaemidio42@gmail.com>
2019-10-31 11:27:46 -0300

    adiciona pasta de exercicio 1f

--- 01f_ajax_giphy_search_input/index.js
+++ 01f_ajax_giphy_search_input/index.js
@@ -7,4 +7,35 @@
 
 const request = new XMLHttpRequest()
 const input = document.getElementById('gifInput')
-console.log('input')
+console.log('input');
+
+const botao = document.querySelector('#gifButton');
+
+botao.addEventListener('click', function(evento){
+    fazerAlgoDepoisDeClicar(evento)
+
+});
+
+function fazerAlgoDepoisDeClicar(evento){
+    evento.preventDefault();
+    const valorDoInput = input.nodeValue;
+    // console.log(valorDoInput);
+
+    const minhaChave = 'dc6zaTOxFJmzC'
+    const oQueQueroBuscar = valorDoInput;
+    const enderecoAPI = `http://api.giphy.com/v1/gifs/search?api_key=${minhaChave}&q=${oQueQueroBuscar}&limit=3`;
+    console.log(enderecoAPI);
+    
+
+    
+    const request = new XMLHttpRequest()
+    request.open('get', enderecoAPI);
+    request.send();
+    request.onreadystatechange = function pegandoResposta() {
+        
+    }
+
+
+
+
+}
